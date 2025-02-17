0e125e450d49f7b2f8a8960d9d5990f8c8de17d7
kanan-logiciel <kanan@logiciel.io>
2025-02-17 15:09:15 +0530

    fix: code modified

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,6 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1 className="text-4xl font-bold text-blue-500">
-          Tailwind CSS is working!
-        </h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
